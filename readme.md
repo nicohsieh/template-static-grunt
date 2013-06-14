71bde1434b3c26cb893abaf13a3a455d4bce382a
Kyle Phillips <kyle@haptic-data.com>
2013-06-14 13:02:54 -0700

    initial import

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Static-grunt Template
+_This template is for use in [Sketchplate](http://github.com/hapticdata/sketchplate), but can be used without._
+
+The _static-grunt_ template uses [grunt](http://gruntjs.com) to watch and build [compass + sass](http://compass-style.org), [require.js](http://requirejs.org) and any of the templating engines supported by [consolidate.js](http://github.com/visionmedia/consolidate.js).
+By default the template is setup with [jade](http://github.com/visionmedia/jade) templates.
+
---
+++ template/Gruntfile.js
@@ -0,0 +1,105 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		// compile .scss/.sass to .css using Compass
+		// http://compass-style.org/help/tutorials/configuration-reference/#configuration-properties
+		compass: {
+			dist: {
+				options: {
+					cssDir: 'public/stylesheets',
+					sassDir: 'sass'
+				}
+			}
+		},
+		// specifying JSHint options and globals
+		// https://github.com/cowboy/grunt/blob/master/docs/task_lint.md#specifying-jshint-options-and-globals
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'public/javascripts/**/*.js'
+			],
+			options: {
+				es5: true, //use ecmascript5 mode
+				curly: true, //use curly braces even on one-liners
+				eqeqeq: true, //use strict equality (===, !==)
+				immed: true, //wrap self-invoking functions in parentheses
+				newcap: true, //capitalize first letter of all constructor functions (i.e. new MyObject())
+				noarg: true, //prohibit the use of arguments.caller & arguments.callee (which are forbidden in strict mode of es5+)
+				sub: true, //supress warnings for using brace syntax instead of dot syntax on objects
+				undef: true, //prohibit the use of undeclared variables (spot leaks + globals)
+				eqnull: true, //suppress warnings about using "== null"
+				browser: true, //define globals exposed by modern browsers
+				globals: {
+					module: true, //commonjs global
+					define: true, //AMD global
+					require: true,//AMD + CommonJS global
+					console: true //turn this to false when supporting real-old browsers
+				}
+			}
+		},
+		//compile requirejs files for production
+		requirejs: {
+			compile: {
+				options: {
+					appDir: 'www-dev/',
+					baseUrl: 'javascripts',
+					dir: 'dist/',
+					optimizeCss: 'standard',
+					optimize: 'none',
+					//findNestedDependencies: true,
+					preserveLicenseComments: false,
+					//re-route libs to top-level
+					paths: {},
+					pragmasOnSave: {
+						excludeJade: true
+					},
+					modules: [
+						{
+							name: 'app/main',
+							exclude: [
+								'jquery',
+								'backbone',
+								'underscore'
+							]
+						}
+					]
+				}
+			}
+		},
+		//better-than-watch
+		watch: {
+            options: {
+                livereload: true
+            },
+			stylesheets: {
+				files: [
+					'sass/*.{scss,sass}'
+				],
+				tasks: 'compass'
+			},
+            views: {
+                files: [
+                    'views/*'
+                ],
+                tasks: 'template'
+            }
+		},
+        //watch templates using consolidated.js
+        //setup for jade but can use many templating engines: https://github.com/rockwood/grunt-templater#supported-template-engines
+        template: {
+            index: {
+                src: 'views/index.jade',
+                dest: 'public/index.html',
+                variables: {
+                    title: "Index - Sketchplate",
+                    pretty: true
+                }
+            }
+        }
+	});
+    grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-templater');
+	grunt.registerTask('default', ['compass', 'template','jshint','watch']);
+};
